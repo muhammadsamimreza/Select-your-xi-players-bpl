8eb927172a60d050567eba8b3b9e0d1706479809
muhammad samim reza <muhammadsamimreza@gmail.com>
2025-09-22 18:06:41 +0600

    Player Detais Display functionality done

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -16,19 +16,34 @@ const Player = ({ player }) => {
     <div>
       <div className="card bg-base-100 shadow-sm">
         <figure>
-          <img className="w-full h-[200px] object-cover"
+          <img className="w-full h-[200px] object-cover p-4 rounded-[20px]"
             src={image}
             alt="Player-Image"
           />
         </figure>
-        <div className="card-body">
+        <div className="p-4 space-y-2">
           <h2 className="card-title">{name}</h2>
-          <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
-          </p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+          <div className="flex justify-between items-center">
+           <div className="flex items-center gap-3">
+            <span><img className="w-5" src={flag_image} alt="" /></span>
+           <span>{country}</span>
+           </div>
+           <span className="btn">{category} </span>
+          </div>
+          <div className="flex items-center justify-between font-semibold">
+            <span>Rating</span>
+            <span>{rating}</span>
+          </div>
+          <div className="flex items-center justify-between">
+            <span className="font-semibold">Batting Style</span>
+            <span>{batting_style}</span>
+          </div>
+          <div className="flex justify-between items-center">
+            <div className="font-bold">
+                <span>$</span>
+                <span>{price}</span>
+            </div>
+            <button className="btn">Buy Now</button>
           </div>
         </div>
       </div>
